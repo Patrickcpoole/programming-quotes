8ef367273118443ccf6e6ca51e5d53f82de899a6
Patrick Poole <patrickcpoole@gmail.com>
2019-10-22 23:35:05 -0600

    added basic filtering and sorting methods

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,104 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="quote-container">
+    <input v-on:keyup="filterProgrammingQuotes" v-model="searchTerm" id="search">
+    <table class="uk-table uk-table-striped uk-table-middle">
+      <thead>
+        <tr>
+          <th v-on:click="sortAuthors">author</th>
+          <th v-on:click="sortQuotes">quote</th>
+          <th v-on:click="sortRatings">rating</th>
+        </tr>
+      </thead>
+      <tbody id="tableData">
+        <tr v-for="(programmingQuote, i) in programmingQuotes" v-bind:key="i">
+          <td>{{programmingQuote.author}}</td>
+          <td>"{{programmingQuote.en}}"</td>
+          <td>{{programmingQuote.rating}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      programmingQuotes: [],
+      sortAuthorDirection: false,
+      sortQuotesDirection: false,
+      sortRatingsDirection: false,
+      searchTerm: null
+    };
+  },
+  methods: {
+    loadTableData() {
+      fetch(`https://programming-quotes-api.herokuapp.com/quotes`)
+        .then(response => response.json())
+        .then(data => {
+          this.programmingQuotes = data;
+          console.log(this.programmingQuotes);
+        });
+    },
+    filterProgrammingQuotes() {
+      this.programmingQuotes = this.programmingQuotes.filter(
+        programmingQuote => {
+          let authorString = programmingQuote["author"].toLowerCase();
+          console.log(authorString.indexOf(this.searchTerm));
+
+          return authorString.indexOf(this.searchTerm) !== -1;
+        }
+      );
+    },
+    sortAuthors() {
+      this.sortAuthorDirection = !this.sortAuthorDirection;
+
+      this.programmingQuotes = this.programmingQuotes.sort((a, b) => {
+        return this.sortAuthorDirection
+          ? a["author"] > b["author"]
+            ? 1
+            : -1
+          : a["author"] < b["author"]
+          ? 1
+          : -1;
+      });
+      console.log(this.programmingQuotes);
+    },
+    sortQuotes() {
+      this.sortQuotesDirection = !this.sortQuotesDirection;
+
+      this.programmingQuotes = this.programmingQuotes.sort((a, b) => {
+        return this.sortQuotesDirection
+          ? a["en"] > b["en"]
+            ? 1
+            : -1
+          : a["en"] < b["en"]
+          ? 1
+          : -1;
+      });
+    },
+    sortRatings() {
+      this.sortRatingsDirection = !this.sortRatingsDirection;
+
+      this.programmingQuotes = this.programmingQuotes.sort((a, b) => {
+        return this.sortRatingsDirection
+          ? a["rating"] > b["rating"]
+            ? 1
+            : -1
+          : a["rating"] < b["rating"]
+          ? 1
+          : -1;
+      });
+    }
+  },
+  mounted() {
+    this.loadTableData();
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +116,14 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.quote-container {
+  width: 80%;
+  height: 80vh;
+}
+
+th {
+  text-align: center;
 }
 </style>
